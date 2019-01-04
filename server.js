6b256ef45fbde183e6d9f37e5f1ba4ee6a699bee
Emelie Straus <mlestraus@gmail.com>
2019-01-03 22:51:38 -0700

    dude 2

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ var express = require('express');
 var connection = require('./config/connection')
 var app = express()
 var port = process.env.PORT || 3000
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World! dude'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
